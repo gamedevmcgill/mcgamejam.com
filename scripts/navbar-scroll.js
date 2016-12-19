b38aedcae39da73a98fae258bc03017d3d0d5fe7
erick <erick@hotmail.ca>
2016-12-19 00:20:49 -0500

    Change navbar darkening animation
    Bar now darkens as soon as user scrolls down in an ease-in fashion.

--- scripts/navbar-scroll.js
+++ scripts/navbar-scroll.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
 
-        if (scroll >= 400) {
+        if (scroll >= 100) {
             $(".navbar-fixed-top").removeClass('clear-nav').addClass('dark-nav');
         } else {
             $(".navbar-fixed-top").addClass('clear-nav').addClass('dark-nav');
