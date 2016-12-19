ef83083d68b756273d6814c2ffeeacc60eada264
erick <erick@hotmail.ca>
2016-12-19 13:11:07 -0500

    Fix responsive navbar
    - Made navbar darkening consistent for screen widths
    - Changed positioning of language elements
    - Fixed bug where collapsed navbar wouldn't trigger
    - Centered collapsed navbar's elements

--- scripts/navbar-scroll.js
+++ scripts/navbar-scroll.js
@@ -1,8 +1,24 @@
 $(document).ready(function(){
+
+    //if screen is narrow on document load, load dark navbar
+    if ($(window).width() < 768) {
+        $(".navbar-fixed-top").removeClass('clear-nav').addClass('dark-nav');
+    }
+
+    //toggle between clear and dark navbars depending on screen width
+    $(window).resize(function() {
+        if ($(window).width() < 768) {
+            $(".navbar-fixed-top").removeClass('clear-nav').addClass('dark-nav');
+        } else {
+            $(".navbar-fixed-top").addClass('clear-nav').addClass('dark-nav');
+        }
+    });
+
+    //toggle between clear and dark navbars depending on how much you scroll (only for wider screens)
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
 
-        if (scroll >= 100) {
+        if (scroll >= 100 || $(window).width() < 768) {
             $(".navbar-fixed-top").removeClass('clear-nav').addClass('dark-nav');
         } else {
             $(".navbar-fixed-top").addClass('clear-nav').addClass('dark-nav');
