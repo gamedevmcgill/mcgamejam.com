7e5bbf7a363c30e0ff1174f6bda567a4fb39f3b9
Andreiskate2 <andrei.ungur@mail.mcgill.ca>
2016-12-22 17:41:39 -0500

    Testing JSON requests
    Not functional, just testing it out

--- scripts/write-data.js
+++ scripts/write-data.js
@@ -1,10 +1,33 @@
 var ENGLISH
-function writeEnglish(){
-	$.getJSON( "./scripts/languages/english.json", function(){})
-	.done(function(data){
-		ENGLISH=data;
-		jAlert(ENGLISH);
-	})
-	.always(function(){
-	})
-}
+var FRENCH
+
+$(document).ready(function() {
+	$.ajaxSetup({ cache: false });
+});
+
+$.ajaxSetup({
+			cache: false
+	});
+
+$.getJSON( "/scripts/languages/english.json"+'?', { cache: false},function() {})
+	.done(function( data ) {
+		ENGLISH = data;
+		})
+	.fail( function(d, textStatus, error) {
+        console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+    })
+	.always(function() {
+	});
+
+$.getJSON( "/scripts/languages/french.json"+'?', { cache: false},function() {})
+	.done(function( data ) {
+		FRENCH = data;
+		})
+	.fail( function(d, textStatus, error) {
+        console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+    })
+	.always(function() {
+	});
+$('.whatisthis').click(function(){
+	$('.test').append(ENGLISH.more);
+});
