abe1d809cc20d3abf4c52383216b6918873fdeac
Andreiskate2 <andrei.ungur@mail.mcgill.ca>
2016-12-22 18:37:18 -0500

    Languages functional
    Languages are now both functional. It is just missing extensive testing

--- scripts/write-data.js
+++ scripts/write-data.js
@@ -33,9 +33,31 @@ $.getJSON( "./scripts/languages/french.json"+'?', { cache: false},function() {})
 
 $('.selecten').click(function(){
 	$('#title-tagline').text(ENGLISH.motto);
+	$('#learn').text(ENGLISH.more);
+	$('#parallax-1 h2').text(ENGLISH.what);
+	$('#abouttext').text(ENGLISH.description);
+	$('#recommend').text(ENGLISH.recommendations);
+	$('#parallax-2 h2').text(ENGLISH.preparation);
+	$('#prep').text(ENGLISH.software);
+	$('#where').text(ENGLISH.actualWhereWhen);
+	$('#parallax-3 h2').text(ENGLISH.signup);
+	$('#interest').text(ENGLISH.interested);
+	$('#footer h4').text(ENGLISH.sponsors);
+
 	
 });
 
 $('.selectfr').click(function(){
+
 	$('#title-tagline').text(FRENCH.motto);
+	$('#learn').text(FRENCH.more);
+	$('#parallax-1 h2').text(FRENCH.what);
+	$('#abouttext').text(FRENCH.description);
+	$('#recommend').text(FRENCH.recommendations);
+	$('#parallax-2 h2').text(FRENCH.preparation);
+	$('#prep').text(FRENCH.software);
+	$('#where').text(FRENCH.actualWhereWhen);
+	$('#parallax-3 h2').text(FRENCH.signup);
+	$('#interest').text(FRENCH.interested);
+	$('#footer h4').text(FRENCH.sponsors);
 });
