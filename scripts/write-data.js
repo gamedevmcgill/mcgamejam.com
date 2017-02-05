30f07628099b342f8b3af2c029ec8e0baa080340
Kartoshka <eli.harfouche@gmail.com>
2017-02-05 10:43:33 -0500

    test for load using vanilla JS

--- scripts/write-data.js
+++ scripts/write-data.js
@@ -129,8 +129,6 @@ function writeFrench(){
 	$('#signup-button').text(FRENCH.buyticket);	
 }
 
-//Default language on load
-$( document ).ready(function() {
-    $e.preventDefault();
+window.onload = function(){
 	writeEnglish();	
-});
+}
